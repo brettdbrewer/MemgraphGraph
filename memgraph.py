f453fd7e418d334ce74c38a605834a14b0a97c60
brettdbrewer <brettdbrewer@hotmail.com>
2023-08-01 10:43:11 -0700

    updated to use new llm_util query

--- memgraph.py
+++ memgraph.py
@@ -2,90 +2,11 @@ from typing import Any, Dict, List
 
 from langchain.graphs import Neo4jGraph
 
-node_query = """
-CALL meta_util.schema(true) YIELD nodes RETURN nodes AS output;
+schema_query = """
+CALL llm_util.schema("prompt_ready")
+YIELD *
+RETURN *
 """
-
-rel_properties_query = """
-CALL meta_util.schema(true) YIELD relationships RETURN relationships AS output;
-"""
-
-rel_query = """
-CALL meta_util.schema() YIELD relationships RETURN relationships AS output;
-"""
-
-
-def build_schema(
-    nodes: List[Dict[str, Any]],
-    rel_properties: List[Dict[str, Any]],
-    relationships: List[Dict[str, Any]],
-) -> str:
-    node_dict: Dict = {}
-    node_prop_str: str = ""
-    rel_prop_str: str = ""
-    rel_str: str = ""
-
-    # iterate over nodes in the graph schema
-    node_list = nodes[0]["output"]
-    for node in node_list:
-        node_label_list = []
-        node_id = node["id"]
-        for node_label in node["labels"]:
-            node_label_list.append(node_label)
-        node_dict[node_id] = node_label_list
-
-        # create list of all node properties
-        node_property_list = list(node["properties"]["properties_count"].keys())
-
-        # form property string(s) for prompt injection
-        node_properties_str = ""
-        for property in node_property_list:
-            node_properties_str += f"""('property': '{property}', 'type': 'STRING'),"""
-
-        # form the full label and property string per node label for prompt injection
-        for node_label in node_dict[node_id]:
-            node_prop_str += f"""Node Name: '{node_label}', 
-            Node Properties: [{node_properties_str}]\n"""
-
-    # form the relationship property string(s) for prompt injection
-    rel_prop_list = rel_properties[0]["output"]
-    for rel in rel_prop_list:
-        rel_property_list = list(rel["properties"]["properties_count"].keys())
-
-        if rel_property_list:  # only add if there are properties
-            # form property string(s) for prompt injection
-            rel_properties_str = ""
-            for property in rel_property_list:
-                if property != "count":  # don't add count property
-                    rel_properties_str += (
-                        f"""('property': '{property}', 'type': 'STRING'),"""
-                    )
-
-                    # form the full label and property string
-                    # per node label for prompt injection
-                    rel_label = rel["label"]
-                    rel_prop_str += f"""Relationship Name: '{rel_label}', 
-                    Node Properties: [{rel_properties_str}]\n"""
-
-    # form the relationship strings for prompt injection
-    rel_list = relationships[0]["output"]
-    # each node may have more than one label, so iterate through
-    # each permutatation of start and end labels for each relationship
-    for rel in rel_list:
-        for start_label in node_dict[rel["start"]]:
-            for end_label in node_dict[rel["end"]]:
-                rel_str += f"['(:{start_label})-[:{rel['label']}]->(:{end_label})']\n"
-
-    schema = f"""
-Node properties are the following:
-{node_prop_str}
-Relationship properties are the following:
-{rel_prop_str}
-The relationships are the following:
-{rel_str}
-    """
-
-    return schema
 
 
 class MemgraphGraph(Neo4jGraph):
@@ -143,10 +64,7 @@ class MemgraphGraph(Neo4jGraph):
         """
         Refreshes the Memgraph graph schema information.
         """
-        node_properties = self.query(node_query)
-        relationships_properties = self.query(rel_properties_query)
-        relationships = self.query(rel_query)
 
-        self.schema = build_schema(
-            node_properties, relationships_properties, relationships
-        )
+        db_schema = self.query(schema_query)[0].get("schema")
+        assert db_schema is not None
+        self.schema = db_schema
